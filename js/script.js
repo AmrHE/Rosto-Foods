6e92b5db9c81f8a64747a551008c9d47f2c5f8d2
AmrHE <amr.hassan.emam@gmail.com>
2022-01-22 23:12:13 +0200

    finalize the project

--- js/script.js
+++ js/script.js
@@ -6,9 +6,28 @@ menu.onclick = () => {
 	navbar.classList.toggle("active");
 };
 
-menu.onscroll = () => {
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header .navbar a");
+
+window.onscroll = () => {
 	menu.classList.remove("fa-times");
 	navbar.classList.remove("active");
+
+	sections.forEach((section) => {
+		let top = window.scrollY;
+		let height = section.offsetHeight;
+		let offset = section.offsetTop - 150;
+		let id = section.getAttribute("id");
+
+		if (top >= offset && top < offset + height) {
+			navLinks.forEach((links) => {
+				links.classList.remove("active");
+				document
+					.querySelector("header .navbar a[href*=" + id + "]")
+					.classList.add("active");
+			});
+		}
+	});
 };
 
 document.querySelector("#search-icon").onclick = () => {
@@ -18,3 +37,52 @@ document.querySelector("#search-icon").onclick = () => {
 document.querySelector("#close").onclick = () => {
 	document.querySelector("#search-form").classList.remove("active");
 };
+
+var swiper = new Swiper(".home-slider", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	loop: true,
+
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		640: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		1024: {
+			slidesPerView: 3,
+		},
+	},
+});
+
+function loader() {
+	document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+	setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
